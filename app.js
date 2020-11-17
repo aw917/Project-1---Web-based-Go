ab52ca1ac7d7a0a1ff19d6d9ce720d5bcebda663
aw917 <aweixel917@gmail.com>
2020-11-17 12:16:09 -0500

    incremental progress

--- app.js
+++ app.js
@@ -64,6 +64,9 @@ const boardDomNode = document.querySelector('.board-div');
 const whiteStone = document.querySelector('.white-stone');
 const blackStone = document.querySelector('.black-stone');
 
+// sound effect
+const stoneSoundEffect = document.querySelector('#board-sound-effect');
+
 
 // ====================
 // Game Classes
@@ -85,12 +88,13 @@ class GameState {
         this.capturedBlackStones;
         this.whitePoints;
         this.blackPoints;
+
         // Pre-set values
         this.komi = 3.5;
         this.boardSize = 9;
     }
     resign() {
-        if (getCurrentTurn() === "white") {
+        if (getCurrentTurn() === "White") {
             alert(`White resigned!  Black won the game!`)
         } else {
             alert(`Black resigned!  White won the game!`)
@@ -103,7 +107,6 @@ class GameState {
             // function for counting stones
         } else {
             this.lastMove = "Pass";
-            // to be created
         }
     }
     playedStone () {
@@ -125,6 +128,7 @@ let moveHolder = [];
 
 // Board DOM nodes
 let boardSquareHolder = [];
+
 // Create DIV's for board squares
 for (let i = 0; i < (game.boardSize * game.boardSize); i++) {
     let boardSquares = document.createElement('div');
@@ -148,7 +152,14 @@ for (let i = 0; i < (game.boardSize * game.boardSize); i++) {
 // Modal play button
 playButton.addEventListener('click', () => {
     modalDisplay.style.display = "none";
+    enableOtherListeners(); // event listeners only accessible once modal is closed
 })
+
+/*
+Enable other listeners prevents eventlisteners from being triggered
+until the modal has been removed (did not indent)
+**/
+const enableOtherListeners = () => {
 
 // Functional buttons
 resignButton.addEventListener('click', game.resign);
@@ -161,8 +172,53 @@ const stoneHighlight = (evt) => {
 
 // Square titles
 for (let i = 0; i < boardSquareHolder.length; i++) {
-    boardSquareHolder[i].addEventListener('pointerenter', (evt) => {
-        evt.target.appendChild(whiteStone);
-        console.log(evt.target);
+    boardSquareHolder[i].addEventListener('click', (evt) => {
+    // Control for spots already played in
+    if (evt.target.className.includes('played')) {
+        console.log('already played');
+    // Logic for white or black
+    } else {
+        if (getCurrentTurn() === "White") {
+            evt.target.setAttribute('class', 'played-white');
+            evt.target.opacity = 1;
+        } else {
+            evt.target.setAttribute('class', 'played-black');
+        }
+        // Increment and then play sound effect is move is available
+        turnTracker ++;
+        stoneSoundEffect.play();
+    }
     })
 }
+
+// Puts hover-over feature
+for (let i = 0; i < boardSquareHolder.length; i++) {
+    boardSquareHolder[i].addEventListener('pointerenter', (evt) => {
+        // Show hover if the area has not been played
+        if (evt.target.className.includes("played")) {
+            // Control for whose turn it is
+            console.log('already played');
+        } else {
+            if (getCurrentTurn() === "White") {
+                evt.target.className = "white-stone";
+                evt.target.style.opacity = 0.7;
+            } else {
+                evt.target.className = "black-stone";
+                evt.target.style.opacity = 0.7;
+            }
+        }
+    })
+
+}
+// Removes hover formatting on exit
+for (let i = 0; i < boardSquareHolder.length; i++) {
+    boardSquareHolder[i].addEventListener('pointerout', (evt) => {
+    if (evt.target.className.includes("played")) {
+        console.log('already played') 
+    } else {
+        evt.target.style.opacity = 0;
+    }
+    })
+}
+
+} // end to all post-modal event listeners
